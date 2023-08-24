81fde77270aa7f8d750d71bae09613219b4267ca
ono212 <qkrwjdals302@naver.com>
2023-08-24 20:40:42 +0900

    Test: 서버 모킹 설정, 주문 준비 완료시 버튼 활성화 테스트 작성

--- src/DirectToBoot.test.tsx
+++ src/DirectToBoot.test.tsx
@@ -1,7 +1,19 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { DirectToBoot } from "./DirectToBoot";
 
+import { createServer, Server } from "miragejs";
+
+let server: Server;
+
 describe("Direct To Boot", () => {
+  beforeEach(() => {
+    server = createServer({});
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
   it("'트렁크로 간편 배송 서비스' 텍스트를 렌더링한다.", () => {
     render(<DirectToBoot orderId="order-id" />);
 
@@ -13,5 +25,27 @@ describe("Direct To Boot", () => {
     const button = screen.getByRole("button");
     expect(button).toBeInTheDocument();
     expect(button).toBeDisabled();
+  });
+
+  it("주문이 준비 완료되면 버튼이 활성화된다.", async () => {
+    server.get("/api/orders/:id", (schema, request) => {
+      return {
+        id: request.params.id,
+        status: "ready",
+      };
+    });
+
+    render(<DirectToBoot orderId="order-id" />);
+
+    expect(screen.getByText("트렁크로 간편 배송 서비스")).toBeInTheDocument();
+    expect(
+      screen.getByText("아직 주문 내역을 준비 중이에요!")
+    ).toBeInTheDocument();
+
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toBeDisabled();
+
+    await waitFor(() => expect(button).toBeEnabled(), { timeout: 3000 });
   });
 });
