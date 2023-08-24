a8919678e53190454ef7f0e5f394c4d913ee6502
ono212 <qkrwjdals302@naver.com>
2023-08-24 19:24:19 +0900

    Feat: 주문 준비 중일 때의 기본 DirectToBoot컴포넌트 구현

---
+++ src/DirectToBoot.tsx
@@ -0,0 +1,9 @@
+export const DirectToBoot = ({ orderId }: { orderId: string }) => {
+  return (
+    <div>
+      <h1>트렁크로 간편 배송 서비스</h1>
+      <p>아직 주문 내역을 준비 중이에요!</p>
+      <button disabled>가게에 도착해있어요!</button>
+    </div>
+  );
+};
