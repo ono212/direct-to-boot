ea0783bc6fd4c30809b97f915ffc150d0d12f4f8
ono212 <qkrwjdals302@naver.com>
2023-08-25 20:21:14 +0900

    Feat: 주문 준비 완료시 화면 구현

--- src/DirectToBoot.tsx
+++ src/DirectToBoot.tsx
@@ -1,9 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function getMessage(status: string) {
+  switch (status) {
+    case "initialized":
+      return "아직 주문 내역을 준비 중이에요!";
+    case "ready":
+      return "가게에 도착하셨다면 버튼을 눌러주세요! 주문하신 물건을 저희 직원이 가져다 드리겠습니다!";
+  }
+}
+
 export const DirectToBoot = ({ orderId }: { orderId: string }) => {
+  const [status, setStatus] = useState<string>("initialized");
+
+  useEffect(() => {
+    axios.get(`/api/orders/${orderId}`).then((res) => {
+      if (res.data.status === "ready") {
+        setStatus("ready");
+      }
+    });
+  }, [orderId]);
+
   return (
     <div>
       <h1>트렁크로 간편 배송 서비스</h1>
-      <p>아직 주문 내역을 준비 중이에요!</p>
-      <button disabled>가게에 도착해있어요!</button>
+      <p>{getMessage(status)}</p>
+      <button disabled={status !== "ready"}>가게에 도착해있어요!</button>
     </div>
   );
 };
