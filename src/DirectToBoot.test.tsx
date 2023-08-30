c8c3b8ec2bddae1c17c61fa63fc354e3b3ca8fde
ono212 <qkrwjdals302@naver.com>
2023-08-30 23:11:41 +0900

    Test: 알림이 전송되지 않았을 경우 폴백 버튼이 보이도록 테스트를 추가한다.

--- src/DirectToBoot.test.tsx
+++ src/DirectToBoot.test.tsx
@@ -103,4 +103,38 @@ describe("Direct To Boot", () => {
       "알려주셔서 감사합니다. 주문이 곧 도착할 예정입니다."
     );
   });
+
+  it("손님이 버튼을 클릭했지만 가게에 알림이 가는 것이 실패할 경우 폴백 버튼이 보인다.", async () => {
+    myRender(<DirectToBoot orderId="unreachable-order-id" />);
+
+    expect(screen.getByText("트렁크로 간편 배송 서비스")).toBeInTheDocument();
+    expect(
+      screen.getByText("아직 주문 내역을 준비 중이에요!")
+    ).toBeInTheDocument();
+
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toBeDisabled();
+
+    await waitFor(() => expect(button).toBeEnabled(), { timeout: 3000 });
+    await screen.findByText(
+      "가게에 도착하셨다면 버튼을 눌러주세요! 주문하신 물건을 저희 직원이 가져다 드리겠습니다!"
+    );
+
+    userEvent.click(button);
+
+    // eslint-disable-next-line testing-library/prefer-find-by
+    await waitFor(
+      () =>
+        expect(
+          screen.getByText("문제가 생겼습니다. 이 번호로 전화를 걸어주세요.")
+        ).toBeInTheDocument(),
+      { timeout: 3000 }
+    );
+
+    const phoneCallButton = screen.getByText("02-123-4567");
+    await waitFor(() => expect(phoneCallButton).toBeInTheDocument(), {
+      timeout: 3000,
+    });
+  });
 });
